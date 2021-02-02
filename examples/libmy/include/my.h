8052fa8d789405a7379c04b7579f3a8f0f27f288
nasso <nassomails@gmail.com>
2021-02-02 19:52:08 +0100

    Remove example

--- examples/libmy/include/my.h
+++
@@ -1,7 +0,0 @@
-#ifndef MY_H_
-#define MY_H_
-
-int my_printf(const char* fmt, ...);
-int my_putstr(const char* str);
-
-#endif /* !MY_H_ */
--- examples/libmy/src/my_putstr.c
+++
@@ -1,6 +0,0 @@
-#include "my.h"
-
-int my_putstr(const char* str)
-{
-    return my_printf("%s", str);
-}
